74ab522156b18c53b5267620f3e30b22b34e8f61
amitchhangani <chhangani.amit@gmail.com>
2016-11-11 23:54:53 +0530

    Changes

--- app/models/collectionCenter.js
+++ app/models/collectionCenter.js
@@ -12,7 +12,7 @@ var CollectionCenterSchema = new Schema({
 	city: {type: Schema.Types.ObjectId, ref: 'City'},
 	created:{type:Number},
 	deleted:{type:Boolean,default:0},
-	vehicle:{type: Schema.Types.ObjectId, ref: 'Vehicle'};
+	vehicle:{type: Schema.Types.ObjectId, ref: 'Vehicle'}
 })
 
 var CollectionCenter = module.exports = mongoose.model('CollectionCenter', CollectionCenterSchema);
